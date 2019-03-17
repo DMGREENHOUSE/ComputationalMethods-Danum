4e4d56cdc17f36cfa0da8ca87e4f96e2e9fc2a3c
calumbinmore <calumbinmore@gmail.com>
2019-03-17 17:20:31 +0800

    recentest

--- main.m
+++ main.m
@@ -28,7 +28,15 @@ function main()
                     acceleration = acceleration + accFromKthPlanet;
                 end
             end
+            %acceleration
+            velocity = [targetPlanet(5), targetPlanet(6), targetPlanet(7)];
             disp(acceleration);
+            %next planet line has updated values
+            derivState = [velocity(1), velocity(2), velocity(3), acceleration(1),...
+                acceleration(2), acceleration(3)];
+            currentState=targetPlanet(2:7);
+            newState=rk4(currentState,t,tau,derivState,1);%1 is GM
+            planetMultiDArray(i+1,:,j) = [100, 0,0,0,0,0,0];
         end
     end
 end
@@ -45,3 +53,27 @@ function accel=gravityAccCalc(M, r)
     coef= -G*M;
     accel = coef*r/norm(r)^3;
 end
+
+function xout=rk4(x,t,tau,derivsRK,param) 
+    %Runge-Kuttaintegrator(4thorder) 
+    %Inputarguments%x=currentvalueofdependentvariable
+    %t=independentvariable(usuallytime) 
+    %tau=stepsize(usuallytimestep)
+    %derivsRK=righthandsideoftheODE;derivsRKisthe 
+    %nameofthefunctionwhichreturnsdx/dt
+    %CallingformatderivsRK(x,t,param).
+    %param=extraparameterspassedtoderivsRK 
+    %Outputarguments
+    %xout=newvalueofxafterastepofsizetau
+    half_tau=0.5*tau; 
+    F0=feval(derivsRK,x,t,param);
+    t_half=t+half_tau; 
+    xtemp=x+half_tau*F0; 
+    F1=feval(derivsRK,xtemp,t_half,param);
+    xtemp=x+half_tau*F1; 
+    F2=feval(derivsRK,xtemp,t_half,param);
+    t_full=t+tau;
+    xtemp=x+tau*F2;
+    F3=feval(derivsRK,xtemp,t_full,param); 
+    xout=x+tau/6.*(F0+F3+2.*(F1+F2)); 
+end; 
